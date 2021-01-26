a90357dbf8920fb83ddb4d02dd5ea1bc49b78f06
LUMOS INC <LUMOS INC@LUMOS>
2021-01-26 17:49:19 +0300

    16 ocak ders sonu

---
+++ SepetManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete Eklendi: " + urun.Adi);
+
+        }
+
+        public void Ekle2(string urunAdi, string acikalma, double fiyat,int stokAdeti)
+        {
+            Console.WriteLine("Sepete Eklendi: " + urunAdi);
+
+
+        }
+
+
+    }
+}
---
+++ Urun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    public class Urun
+    {
+        //property-özellik prop tusuna basıp tap tap yap
+
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public double Fiyati { get; set; }
+        public string Aciklama { get; set; }
+        public int stokAdeti { get; set; }
+
+    }
+}
